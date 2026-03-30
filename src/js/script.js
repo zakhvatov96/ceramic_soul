7e1d3636dc6bbef57bee2411fe2911bf6ccf8c16
Dima <example@gmail.com>
2026-03-31 00:32:08 +0200

    mobile adaptation for slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,40 @@
+import Swiper from 'swiper';
+import {
+	Navigation,
+	Pagination
+} from 'swiper/modules';
+
+
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 import "/src/sass/style.scss";
+
+const swiper = new Swiper(".swiper", {
+	slidesPerView: 1,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.icon-right-open',
+		prevEl: '.icon-left-open',
+	},
+	breakpoints: {
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 5
+		},
+		1920: {
+			slidesPerView: 3,
+			spaceBetween: 35
+		},
+	},
+	modules: [Navigation, Pagination],
+});
