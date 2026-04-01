1387b2e9331bd9b8da92266b77ea10b9d2ee16f9
Dima <example@gmail.com>
2026-04-01 20:26:24 +0200

    burger menu is finished

--- src/js/script.js
+++ src/js/script.js
@@ -15,26 +15,43 @@ import 'swiper/css/pagination';
 
 import "/src/sass/style.scss";
 
-const swiper = new Swiper(".swiper", {
-	slidesPerView: 1,
-	loop: true,
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.icon-right-open',
-		prevEl: '.icon-left-open',
-	},
-	breakpoints: {
-		1200: {
-			slidesPerView: 3,
-			spaceBetween: 5
-		},
-		1920: {
-			slidesPerView: 3,
-			spaceBetween: 35
-		},
-	},
-	modules: [Navigation, Pagination],
+const burger = document.querySelector('.burger'),
+	menu = document.querySelector('.header__menu'),
+	close = document.querySelector('.header__menu-close');
+
+burger.addEventListener('click', () => {
+	menu.classList.add('header__menu_active');
+	document.body.style.overflow = 'hidden';
 });
+
+close.addEventListener('click', () => {
+	menu.classList.remove('header__menu_active');
+	document.body.style.overflow = '';
+})
+
+
+try {
+	const swiper = new Swiper(".swiper", {
+		slidesPerView: 1,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.icon-right-open',
+			prevEl: '.icon-left-open',
+		},
+		breakpoints: {
+			1200: {
+				slidesPerView: 3,
+				spaceBetween: 5
+			},
+			1920: {
+				slidesPerView: 3,
+				spaceBetween: 35
+			},
+		},
+		modules: [Navigation, Pagination],
+	});
+} catch (e) {}
