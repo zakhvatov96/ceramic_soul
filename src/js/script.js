3bdf667f0a01cae38b3a7ed28018059c7bdf58c9
Dima <example@gmail.com>
2026-04-08 00:10:10 +0200

    tabs component is finished

--- src/js/script.js
+++ src/js/script.js
@@ -55,3 +55,21 @@ try {
 		modules: [Navigation, Pagination],
 	});
 } catch (e) {}
+
+try {
+	const tabs = document.querySelectorAll('.catalog__tab'),
+		contents = document.querySelectorAll('.catalog__item');
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener('click', () => {
+			tabs.forEach((t) => t.classList.remove('catalog__tab_active'));
+			contents.forEach((c) => c.style.display = 'none');
+
+			tab.classList.add('catalog__tab_active');
+			contents[index].style.display = 'flex';
+		})
+
+	})
+
+	contents.forEach((c, i) => c.style.display = i === 0 ? 'flex' : 'none');
+} catch (e) {}
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import {
+	dirname,
+	resolve
+} from "node:path";
+import {
+	fileURLToPath
+} from "node:url";
+import {
+	defineConfig
+} from "vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(
+	import.meta.url));
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
+				catalog: resolve(__dirname, "catalog.html"),
 			},
 		},
 	},
